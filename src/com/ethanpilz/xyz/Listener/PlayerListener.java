1c680152bde200c8b152f75246d52d32309f3b55
BuildTools <ethanppilz@gmail.com>
2017-07-17 14:13:17 -0400

    Freeze command no longer freezes yaw and looks super glitchy

--- src/com/ethanpilz/xyz/Listener/PlayerListener.java
+++ src/com/ethanpilz/xyz/Listener/PlayerListener.java
@@ -1,6 +1,7 @@
 package com.ethanpilz.xyz.Listener;
 
 import com.ethanpilz.xyz.XYZ;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,7 +12,9 @@ public class PlayerListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerMove(PlayerMoveEvent event) {
      if(XYZ.freezeManager.isPlayerFrozen(event.getPlayer())){
-         event.setCancelled(true);
+         Location from = event.getFrom();
+         Location to = event.getTo();
+         event.setCancelled(!(from.getX() == to.getX() && from.getY() == to.getY() && from.getZ() == to.getZ()));
      }
     }
 }
